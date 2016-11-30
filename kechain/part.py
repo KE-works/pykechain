cd554b6cbda514ee5285f52987e84f89c21fc6e1
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2016-11-30 15:57:15 +0100

    abstracted part and property commonality to structure class

--- kechain/part.py
+++ kechain/part.py
@@ -1,25 +1,27 @@
+from kechain.structure import Structure
 from kechain.property import Property
 
 
-class Part(object):
-    id = None
-    name = None
-    properties = []
-    _dirty = False
+class Part(Structure):
 
-    @classmethod
-    def from_dict(cls, data):
-        part = cls()
+    def __init__(self):
+        super().__init__()
+        self._properties = []
 
-        part.id = data['id']
-        part.name = data['name']
-
-        part.properties = [Property.from_dict(prop_data) for prop_data in data['properties']]
-
-        return part
+    @property
+    def properties(self):
+        return self._properties
 
     def find_property(self, property_name):
         return next(prop for prop in self.properties if prop.name == property_name)
+
+    @classmethod
+    def from_dict(cls, data):
+        obj = super().from_dict(data)
+
+        obj._properties = [Property.from_dict(prop_data) for prop_data in data['properties']]
+
+        return obj
 
     def __repr__(self):
         props = ', '.join('{0}: {1}'.format(p.name, p.value) for p in self.properties)
