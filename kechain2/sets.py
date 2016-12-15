366ff5843c4cddc4f742b6d012c507469a686b1e
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2016-12-15 11:20:56 +0100

    added optional category display in part set

--- kechain2/sets.py
+++ kechain2/sets.py
@@ -19,18 +19,27 @@ class PartSet(object):
     def _repr_html_(self):
         from .api import api_url
 
+        all_instances = all(p.category == 'INSTANCE' for p in self._parts)
+
         html = []
 
         html.append("<table width=100%>")
-
         html.append("<tr>")
         html.append("<th>Part</th>")
+
+        if not all_instances:
+            html.append("<th>Category</th>")
+
         html.append("<th>ID</th>")
         html.append("</tr>")
 
         for part in self:
             html.append("<tr>")
             html.append("<td>{}</td>".format(part.name))
+
+            if not all_instances:
+                html.append("<td>{}</td>".format(part.category))
+
             html.append("<td><a target='_blank' href={}>".format(api_url('part', part_id=part.id)))
             html.append("{}</a></td>".format(part.id))
             html.append("</tr>")
