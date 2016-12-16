a375b4ca6f0c287eee6e361cdbf075778b75cce6
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2016-12-16 09:30:43 +0100

    added simple test for part

--- kechain2/tests/test_simple.py
+++ kechain2/tests/test_simple.py
@@ -9,11 +9,23 @@ class TestApi(object):
     def test_parts(self):
         from kechain2.api import session
 
-        kechain2.login("***REMOVED***")
-
         with betamax.Betamax(session) as vcr:
             vcr.use_cassette('parts')
+
+            kechain2.login("***REMOVED***")
 
             part_set = kechain2.parts()
 
             assert len(part_set) == 29
+
+    def test_part(self):
+        from kechain2.api import session
+
+        with betamax.Betamax(session) as vcr:
+            vcr.use_cassette('part')
+
+            kechain2.login("***REMOVED***")
+
+            gears = kechain2.part('Bike').property('Gears')
+
+            assert gears.value == 6
