68dcd67c2dd13c4f02ffec59aa5fa45e64863704
Jochem Berends <jochem.berends@ke-works.com>
2016-12-17 17:06:40 +0100

    added some documentation

--- kechain2/client.py
+++ kechain2/client.py
@@ -23,7 +23,19 @@ class Client(object):
         self.headers = {}
         self.auth = None
 
-    def login(self, username=None, password=None, token=None):
+    def login(self, username=None, password=None, token=None, url=None, check_certificate=True):
+        """
+        Login into KE-chain with either username/password or token
+        :param username: username for your user from KE-chain
+        :param password: password for your user from KE-chain
+        :param token: user authentication token retrieved from KE-chain
+        :param url: url where KE-chain lives, defaults to localhost:8000 for development
+        :param check_certificate: checks ssl certificates, set to False to ignore https (self-signed) certificates
+        """
+        if url:
+            self.api_root = url
+        if not check_certificate:
+            self.session = requests.Session(verify=False)
         if token:
             self.headers['Authorization'] = 'Token {}'.format(token)
             self.auth = None
@@ -31,10 +43,12 @@ class Client(object):
             self.headers.pop('Authorization', None)
             self.auth = (username, password)
 
-    def build_url(self, resource, **kwargs):
+    def _build_url(self, resource, **kwargs):
+        """helper method to build the correct API url"""
         return self.api_root + API_PATH[resource].format(**kwargs)
 
     def _request(self, method, url, **kwargs):
+        """helper method to perform the request on the API"""
         r = self.session.request(method, url, auth=self.auth, headers=self.headers, **kwargs)
 
         if r.status_code == 403:
@@ -43,7 +57,18 @@ class Client(object):
         return r
 
     def parts(self, name=None, pk=None, model=None, category='INSTANCE', bucket=None, activity=None):
-        r = self._request('GET', self.build_url('parts'), params={
+        """
+        Retrieve multiple KE-chain Parts
+
+        :param name: filter on name
+        :param pk: filter on primary key
+        :param model: filter on model_id
+        :param category: filter on category_id
+        :param bucket: filter on bucket_id
+        :param activity: filter on activity_id
+        :return: a list of Parts
+        """
+        r = self._request('GET', self._build_url('parts'), params={
             'id': pk,
             'name': name,
             'model': model.id if model else None,
@@ -59,6 +84,17 @@ class Client(object):
         return PartSet((Part(p) for p in data['results']), client=self)
 
     def part(self, *args, **kwargs):
+        """
+        Retrieve single KE-chain Part, if multiple parts are returned if will notify and returns the first part.
+
+        :param name: filter on name
+        :param pk: filter on primary key
+        :param model: filter on model_id
+        :param category: filter on category_id
+        :param bucket: filter on bucket_id
+        :param activity: filter on activity_id
+        :return: a single Part or AssertionError
+        """
         _parts = self.parts(*args, **kwargs)
 
         assert len(_parts) > 0, "No part fits criteria"
