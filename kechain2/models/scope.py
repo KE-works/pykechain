906d92b6845bee763ea0a7f26d15de4e8e100893
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2016-12-19 14:42:02 +0100

    added client reference to base model

--- kechain2/models/scope.py
+++ kechain2/models/scope.py
@@ -3,22 +3,16 @@ from kechain2.models import Base
 
 class Scope(Base):
 
-    def __init__(self, json):
-        super(Scope, self).__init__(json)
+    def __init__(self, json, **kwargs):
+        super(Scope, self).__init__(json, **kwargs)
 
         self.bucket = json.get('bucket', {})
 
     def parts(self, *args, **kwargs):
-        from kechain2.api import parts
-
-        return parts(*args, bucket=self.bucket.get('id'), **kwargs)
+        return self._client.parts(*args, bucket=self.bucket.get('id'), **kwargs)
 
     def part(self, *args, **kwargs):
-        from kechain2.api import part
-
-        return part(*args, bucket=self.bucket.get('id'), **kwargs)
+        return self._client.part(*args, bucket=self.bucket.get('id'), **kwargs)
 
     def model(self, *args, **kwargs):
-        from kechain2.api import model
-
-        return model(*args, bucket=self.bucket.get('id'), **kwargs)
+        return self._client.model(*args, bucket=self.bucket.get('id'), **kwargs)
