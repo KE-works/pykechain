18541eaaecf4bd14d4b9e87410023641de535732
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2016-12-19 13:51:33 +0100

    added tests for client

---
+++ kechain2/tests/test_client.py
@@ -0,0 +1,41 @@
+from kechain2.client import Client
+
+
+class TestClient(object):
+
+    def test_init_default_url(self):
+        client = Client()
+
+        assert client.api_root == 'http://localhost:8000/'
+
+    def test_init_custom_url(self):
+        client = Client(url='http://testing.com:1234')
+
+        assert client.api_root == 'http://testing.com:1234'
+
+    def test_init_no_login(self):
+        client = Client()
+
+        assert 'Authorization' not in client.headers
+        assert client.auth is None
+
+    def test_init_basic_auth(self):
+        client = Client()
+
+        client.login('admin', 'pass')
+
+        assert 'Authorization' not in client.headers
+        assert client.auth
+
+    def test_init_token(self):
+        client = Client()
+
+        client.login(token='123123')
+
+        assert client.headers['Authorization'] == 'Token 123123'
+        assert client.auth is None
+
+    def test_init_no_ssl(self):
+        client = Client(check_certificates=False)
+
+        assert client.session.verify is False
