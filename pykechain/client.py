7fdec81e02461b4e198eb5bf50849a4ba16cfffa
Jochem Berends <jochem.berends@ke-works.com>
2016-12-21 23:00:10 +0100

    added custom exeptions, update the readme and the documentation

--- pykechain/client.py
+++ pykechain/client.py
@@ -1,6 +1,6 @@
 import requests
 
-from .exceptions import LoginRequiredError
+from .exceptions import LoginRequiredError, NotFoundError, MultipleFoundError, MultipleFoundWarning
 from .models import Scope, Activity, Part, Property
 from .sets import PartSet
 
@@ -18,6 +18,20 @@ API_PATH = {
 class Client(object):
 
     def __init__(self, url='http://localhost:8000/', check_certificates=True):
+        """
+        The KE-chain 2 python client to connect to a KE-chain (version 2) instance.
+
+        :param url: the url of the KE-chain instance to connect to (defaults to http://localhost:8000)
+        :param check_certificates: if to check TLS/SSL Certificates. Defaults to True
+
+        Examples:
+        ---------
+        >>> from pykechain import Client
+        >>> kec = Client(url='https://default-tst.localhost:9443', check_certificates=False)
+
+        >>> from pykechain import Client
+        >>> kec = Client()
+        """
         self.session = requests.Session()
         self.api_root = url
         self.headers = {}
@@ -33,6 +47,18 @@ class Client(object):
         :param username: username for your user from KE-chain
         :param password: password for your user from KE-chain
         :param token: user authentication token retrieved from KE-chain
+
+        Examples:
+        ---------
+        Token Authentication::
+        >>> from pykechain import Client
+        >>> kec = Client()
+        >>> kec.login(Token='<some-super-long-secret-token-you-download-from-your-kechain-user-profile')
+
+        Basic Authentication::
+        >>> from pykechain import Client
+        >>> kec = Client()
+        >>> kec.login(username='demo_user', password='visible_password')
         """
         if token:
             self.headers['Authorization'] = 'Token {}'.format(token)
@@ -55,41 +81,81 @@ class Client(object):
         return r
 
     def scopes(self, name=None, status='ACTIVE'):
+        """
+        Returns all scopes visible / accessible for the logged in user
+
+        :param name: if provided, filter the search for a scope/project by name
+        :param status: if provided, filter the search for the status. eg. 'ACTIVE', 'TEMPLATE', 'LIBRARY'
+        :return: list of scopes
+        :rtype: list of Scope
+        """
         r = self._request('GET', self._build_url('scopes'), params={
             'name': name,
             'status': status
         })
 
-        assert r.status_code == 200, "Could not retrieve scopes"
+        if r.status_code == 200:
+            raise NotFoundError(msg="Could not retrieve scopes")
 
         data = r.json()
 
         return [Scope(s) for s in data['results']]
 
     def scope(self, *args, **kwargs):
+        """
+        Returns a single scope based on the provided name.
+        Returns a AssertionError if no scopes could be found or multiple scopes are retrieved
+
+        :param args:
+        :param kwargs:
+        :return: a single Scope
+        :rtype: Scope
+        """
         _scopes = self.scopes(*args, **kwargs)
 
-        assert len(_scopes) > 0, "No scope fits criteria"
-        assert len(_scopes) == 1, "Multiple scopes fit criteria"
+        if len(_scopes) == 0:
+            raise NotFoundError(msg="No scope fits criteria")
+        if len(_scopes) != 1:
+            raise MultipleFoundError(msg="Multiple scopes fit criteria")
 
         return _scopes[0]
 
     def activities(self, name=None):
+        """
+        Searches on activities with optional name filter
+        If no activity could be found, returns an Exception
+
+        :param name: filter the activities on Name
+        :return: list of activities
+        :rtype: list of Activity
+        """
         r = self._request('GET', self._build_url('activities'), params={
             'name': name
         })
 
-        assert r.status_code == 200, "Could not retrieve activities"
+        if r.status_code == 200:
+            raise NotFoundError(msg="Could not retrieve activities")
 
         data = r.json()
 
         return [Activity(a) for a in data['results']]
 
     def activity(self, *args, **kwargs):
+        """
+        searches for a single activity
+        returns and error if the activity name is not found, or multiple activities are returned
+
+        :param name: Name of the activity to search
+        :return: Activity with name
+        :rtype: Activity
+        """
         _activities = self.activities(*args, **kwargs)
 
-        assert len(_activities) > 0, "No activity fits criteria"
-        assert len(_activities) == 1, "Multiple activities fit criteria"
+        if len(_activities) == 0:
+            raise NotFoundError(msg="No activity fits criteria")
+        # TODO: could also be a warning (see warnings.warn)
+        if len(_activities) != 1:
+            raise MultipleFoundError(msg="Multiple activities fit criteria")
 
         return _activities[0]
 
@@ -100,10 +166,11 @@ class Client(object):
         :param name: filter on name
         :param pk: filter on primary key
         :param model: filter on model_id
-        :param category: filter on category_id
+        :param category: filter on category (INSTANCE, MODEL, None)
         :param bucket: filter on bucket_id
         :param activity: filter on activity_id
         :return: a list of Parts
+        :rtype: PartSet
         """
         r = self._request('GET', self._build_url('parts'), params={
             'id': pk,
@@ -114,7 +181,8 @@ class Client(object):
             'activity_id': activity
         })
 
-        assert r.status_code == 200, "Could not retrieve parts"
+        if r.status_code == 200:
+            raise NotFoundError(msg="Could not retrieve parts")
 
         data = r.json()
 
@@ -131,11 +199,15 @@ class Client(object):
         :param bucket: filter on bucket_id
         :param activity: filter on activity_id
         :return: a single Part or AssertionError
+        :rtype: Part
         """
         _parts = self.parts(*args, **kwargs)
 
-        assert len(_parts) > 0, "No part fits criteria"
-        assert len(_parts) == 1, "Multiple parts fit criteria"
+        if len(_parts) == 0:
+            raise NotFoundError(msg="No part fits criteria")
+        # TODO: could also be a warning (see warnings.warn)
+        if len(_parts) != 1:
+            raise MultipleFoundError(msg="Multiple parts fit criteria")
 
         return _parts[0]
 
@@ -143,18 +215,30 @@ class Client(object):
         kwargs['category'] = 'MODEL'
         _parts = self.parts(*args, **kwargs)
 
-        assert len(_parts) > 0, "No model fits criteria"
-        assert len(_parts) == 1, "Multiple models fit criteria"
+        if len(_parts) == 0:
+            raise NotFoundError(msg="No model fits criteria")
+        # TODO: could also be a warning (see warnings.warn)
+        if len(_parts) != 1:
+            raise MultipleFoundError(msg="Multiple model fit criteria")
 
         return _parts[0]
 
     def properties(self, name=None, category='INSTANCE'):
+        """
+        retrieves properties by name
+
+        :param name: name to limit the search for.
+        :param category: filter the properties by category. Defaults to INSTANCE. Other options MODEL or None
+        :return: list of properties
+        :rtype: list of Property
+        """
         r = self._request('GET', self._build_url('properties'), params={
             'name': name,
             'category': category
         })
 
-        assert r.status_code == 200, "Could not retrieve properties"
+        if r.status_code == 200:
+            raise NotFoundError(msg="Could not retrieve properties")
 
         data = r.json()
 
