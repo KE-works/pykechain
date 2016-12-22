d5b7b920a63308c66a7cbe949058a78f6942c09c
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2016-12-22 14:36:55 +0100

    updated license classifier

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'Topic :: Software Development :: Libraries',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache Open Source License 2.0',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
