814beb8ac4144ede7c7fa6d40210e55362a70dd9
Jochem Berends <jochem.berends@ke-works.com>
2016-12-28 18:28:35 +0100

    removing the ***secret*** from the github, refactoring into a local .env file. Adding also a .env.template

--- tests/utils.py
+++ tests/utils.py
@@ -1,39 +1,15 @@
-import os
+from envparse import Env
 
-import pytest
-from betamax import Betamax
+# reads a local .env file with the TEST_TOKEN=<user token>
+# ensure that this file is not commited to github (never ever)
 
-from pykechain import Client
+env = Env()
+env.read_envfile()
 
-TEST_URL = 'https://kec2api.ke-chain.com'
-TEST_USER = 'pykechain'  # LVL1
-TEST_TOKEN = '***REMOVED***'
-TEST_SCOPE_ID = 'b9e3f77b-281b-4e17-8d7c-a457b4d92005'
-
-
-# class BaseAPITest(object):
-#     """
-#     Base API test class for Betamax enabled KEC API tests
-#     """
-#
-#     def __init__(self):
-#         """
-#         Initialises and exposes the client (pykechain.Client) connected to TEST_URL
-#         Initialises and exposes betamax vcr on the client.
-#
-#         Example
-#         -------
-#
-#         def test_foo_bar(self):
-#             with self.betamax as vcr:
-#                 vcr.use_cassette(__name__)
-#                 self.client.login(token=TEST_TOKEN)
-#                 # do testing here
-#
-#         """
-#         self.client = Client(url=TEST_URL)
-#         self.betamax = Betamax(self.client)
-#         self.betamax.configure().cassette_library_dir = BETAMAX_CASSETTES_DIR
+TEST_URL = env('TEST_URL', default='https://kec2api.ke-chain.com')
+TEST_USERNAME = env('TEST_USERNAME', default='pykechain')  # LVL1
+TEST_TOKEN = env('TEST_TOKEN')
+TEST_SCOPE_ID = env('TEST_SCOPE_ID', default='b9e3f77b-281b-4e17-8d7c-a457b4d92005')
 
 
 def get_method_name():
