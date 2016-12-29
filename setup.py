4147abf2e6759f7b5ad07d0844302177b421d980
Jochem Berends <jochem.berends@ke-works.com>
2016-12-29 09:18:20 +0100

    adding 3.6 as a test and updating the readme with a supported python versions on pypi

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy'
 
         # OS
         'Operating System :: MacOS',
