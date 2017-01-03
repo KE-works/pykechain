9798f089a8a3d79c74d2f69c359b477e9174168c
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2017-01-03 10:34:02 +0100

    [flake8] removed F401 ignore and re-added statistics

--- pykechain/__init__.py
+++ pykechain/__init__.py
@@ -1,1 +1,5 @@
 from .client import Client
+
+__all__ = [
+    'Client'
+]
--- pykechain/models/__init__.py
+++ pykechain/models/__init__.py
@@ -1,6 +1,15 @@
 from .base import Base
-from .part import Part
-from .partset import PartSet
-from .property import Property
 from .scope import Scope
 from .activity import Activity
+from .part import Part
+from .property import Property
+from .partset import PartSet
+
+__all__ = [
+    'Base',
+    'Scope',
+    'Activity',
+    'Part',
+    'Property',
+    'PartSet'
+]
