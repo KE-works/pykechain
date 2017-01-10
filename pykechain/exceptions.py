d488af05e153ea0f720d00b9d46aa28b2a183efd
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2017-01-10 17:13:18 +0100

    docstyle fixes for exceptions, module __all__ back to square brackets

--- pykechain/exceptions.py
+++ pykechain/exceptions.py
@@ -1,29 +1,26 @@
 
 class APIError(Exception):
-    """
-    A general API Error
+    """A general API Error.
 
     A end-user descriptive message is required
     """
+
     pass
 
 
 class LoginRequiredError(APIError):
-    """
-    A login is required
-    """
+    """A login is required."""
+
     pass
 
 
 class MultipleFoundError(APIError):
-    """
-    Multiple objects are found, while a single object is requested
-    """
+    """Multiple objects are found, while a single object is requested."""
+
     pass
 
 
 class NotFoundError(APIError):
-    """
-    Object not found error
-    """
+    """Object not found error."""
+
     pass
