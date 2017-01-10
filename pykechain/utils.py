6a1040918298d88cdf7ec40e78461abf6d8c7df5
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2017-01-10 17:14:23 +0100

    docstyle added for utils

--- pykechain/utils.py
+++ pykechain/utils.py
@@ -1,4 +1,5 @@
 
 
 def find(iterable, predicate):
+    """Return the first item in the iterable that matches the predicate function."""
     return next((i for i in iterable if predicate(i)), None)
