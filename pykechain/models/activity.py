61116ba430ae243fead9d61ed2e7a845e06aabea
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2017-01-12 14:41:30 +0100

    added docstring to activity

--- pykechain/models/activity.py
+++ pykechain/models/activity.py
@@ -2,11 +2,17 @@ from pykechain.models import Base
 
 
 class Activity(Base):
+    """A virtual object representing a KE-chain activity."""
 
     def __init__(self, json, **kwargs):
+        """Construct an Activity from a json object."""
         super(Activity, self).__init__(json, **kwargs)
 
         self.scope = json.get('scope')
 
     def parts(self, *args, **kwargs):
+        """Retrieve parts belonging to this activity.
+
+        See :class:`pykechain.Client.parts` for available parameters.
+        """
         return self._client.parts(*args, activity=self.id, **kwargs)
