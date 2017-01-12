aea87912a8bf1661f2617e8cf6001e4e9bc4a8d4
Wilco Schoneveld <wilco.schoneveld@ke-works.com>
2017-01-12 14:27:20 +0100

    added docstrings to part

--- pykechain/models/part.py
+++ pykechain/models/part.py
@@ -4,8 +4,10 @@ from pykechain.utils import find
 
 
 class Part(Base):
+    """A virtual object representing a KE-chain part."""
 
     def __init__(self, json, **kwargs):
+        """Construct a part from provided json data."""
         super(Part, self).__init__(json, **kwargs)
 
         self.category = json.get('category')
@@ -14,6 +16,12 @@ class Part(Base):
         self.properties = [Property.create(p, client=self._client) for p in json['properties']]
 
     def property(self, name):
+        """Retrieve property belonging to this part.
+
+        :param name: property name to search for
+        :return: a single :class:`pykechain.models.Property`
+        :raises: NotFoundError
+        """
         found = find(self.properties, lambda p: name == p.name)
 
         if not found:
@@ -22,12 +30,25 @@ class Part(Base):
         return found
 
     def add(self, model, **kwargs):
+        """Add a new child to this part.
+
+        :param model: model to use for the child
+        :return: :class:`pykechain.models.Part`
+        :raises: APIError
+        """
         return self._post_instance(self, model, **kwargs)
 
     def add_to(self, parent, **kwargs):
+        """Add a new instance of this model to a part.
+
+        :param parent: part to add the new instance to
+        :return: :class:`pykechain.models.Part`
+        :raises: APIError
+        """
         return self._post_instance(parent, self, **kwargs)
 
     def delete(self):
+        """Delete this part."""
         r = self._client._request('DELETE', self._client._build_url('part', part_id=self.id))
 
         if r.status_code != 204:
