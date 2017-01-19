0bf7ae410dfc76e4a3f363203598fe152916cf4a
Jochem Berends <jochem.berends@ke-works.com>
2017-01-19 10:00:06 +0100

    started with cli filuploader

---
+++ scripts/file_uploader.py
@@ -0,0 +1,28 @@
+#!/bin/env python
+"""
+Commandline script to upload any file to KE-chain
+
+
+
+
+"""
+
+
+
+import sys
+
+from pykechain import client
+import click
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
