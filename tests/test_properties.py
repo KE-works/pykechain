61ebde27f0f6b7b213d2f29a2a8f277c465a09dd
Wilco Schoneveld <schoneveld.wj@gmail.com>
2017-04-01 15:48:32 +1100

    updated default TEST_SCOPE_NAME and all properties test for new fixture

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -4,37 +4,37 @@ from tests.classes import TestBetamax
 
 class TestProperties(TestBetamax):
     def test_retrieve_properties(self):
-        properties = self.client.properties('Diameter')
+        properties = self.client.properties('Float')
 
         assert len(properties)
 
     def test_get_property(self):
-        bike = self.project.part('Bike')
+        bike = self.project.part('One')
 
-        self.assertEqual(bike.property('Gears').value, 10)
+        self.assertEqual(bike.property('Integer').value, 10)
 
     def test_get_invalid_property(self):
-        bike = self.project.part('Bike')
+        bike = self.project.part('One')
 
         with self.assertRaises(NotFoundError):
             bike.property('Price')
 
     def test_set_property(self):
-        gears = self.project.part('Bike').property('Gears')
+        int = self.project.part('One').property('Integer')
 
-        gears.value = 5
+        int.value = 5
 
-        self.assertEqual(gears.value, 5)
+        self.assertEqual(int.value, 5)
 
-        gears.value = 2
+        int.value = 2
 
-        self.assertEqual(self.project.part('Bike').property('Gears').value, 2)
+        self.assertEqual(self.project.part('One').property('Integer').value, 2)
 
-        gears.value = 10
+        int.value = 10
 
     def test_property_to_part(self):
-        bike = self.project.part('Bike')
+        bike = self.project.part('One')
 
-        bike2 = bike.property('Gears').part
+        bike2 = bike.property('Float').part
 
         self.assertEqual(bike.id, bike2.id)
