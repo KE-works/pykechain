f32b16d69c5f2daee9e68511d56855999ea04e95
Wilco Schoneveld <schoneveld.wj@gmail.com>
2017-04-01 16:34:16 +1100

    fixed all test_parts to work with pykechain fixture

--- tests/test_parts.py
+++ tests/test_parts.py
@@ -90,51 +90,43 @@ class TestParts(TestBetamax):
 
     # version 1.1.3 and later
     def test_retrieve_parent_of_part(self):
-        frame = self.project.part('Frame')  # type:Part
-        assert hasattr(frame, 'parent_id')
-        parent_of_frame = frame.parent()
-        assert type(parent_of_frame) is type(frame)
+        one = self.project.part('One')  # type:Part
+        parent = one.parent()
+        assert type(parent) is Part
 
     def test_retrieve_children_of_part(self):
-        bike = self.project.part('Bike')  # type:Part
-        assert type(bike) is Part
-        children_of_bike = bike.children()
-        assert type(children_of_bike) is PartSet
-        assert len(children_of_bike) >= 1  # eg. Wheels ...
+        product = self.project.part('Product')  # type:Part
+        children = product.children()
+        assert type(children) is PartSet
+        assert len(children) >= 1
 
     def test_retrieve_siblings_of_part(self):
         """Test if the siblings of a part is the same as the children of the parent of the part"""
-        frame = self.project.part('Frame')  # type:Part
-        siblings_of_frame = frame.siblings()
-        assert type(siblings_of_frame) is PartSet
-        assert len(siblings_of_frame) >= 1  # eg. Wheels ...
+        one = self.project.part('One')  # type:Part
+        siblings_of_one = one.siblings()
+        assert type(siblings_of_one) is PartSet
+        assert len(siblings_of_one) >= 1
 
         # double check that the children of the parent of frame are the same as the siblings of frame
-        children_of_parent_of_frame = frame.parent().children()  # type: PartSet
-        assert len(children_of_parent_of_frame) == len(siblings_of_frame)
-        children_of_parent_of_frame_ids = [p.id for p in children_of_parent_of_frame]
-        for sibling in siblings_of_frame:
-            assert sibling.id in children_of_parent_of_frame_ids, \
+        children_of_parent = one.parent().children()  # type: PartSet
+        assert len(children_of_parent) == len(siblings_of_one)
+        children_of_parent_ids = [p.id for p in children_of_parent]
+        for sibling in siblings_of_one:
+            assert sibling.id in children_of_parent_ids, \
                 'sibling {} is appearing in the siblings method and not in the children of ' \
                 'parent method'.format(sibling)
 
     def test_retrieve_part_without_parent_id(self):
-        # only the root does not have a parent_id
-        ROOT_NODE_ID = 'f521333e-a1ed-4e65-b166-999f91a38cf1'
-        root_node = self.project.part(pk=ROOT_NODE_ID)  # type: Part
-        assert hasattr(root_node, 'parent_id')
-        assert root_node.parent_id == None
+        root_node = self.project.part('Product').parent()  # type: Part
+        assert not root_node.parent_id
 
     def test_retrieve_parent_of_part_without_parent_id(self):
-        # only the root does not have a parent_id
-        ROOT_NODE_ID = 'f521333e-a1ed-4e65-b166-999f91a38cf1'
-        root_node = self.project.part(pk=ROOT_NODE_ID)  # type: Part
+        root_node = self.project.part('Product').parent()  # type: Part
         parent_of_rootnode = root_node.parent()
         assert parent_of_rootnode is None
 
     def test_retrieve_siblings_of_part_without_parent_id(self):
-        ROOT_NODE_ID = 'f521333e-a1ed-4e65-b166-999f91a38cf1'
-        root_node = self.project.part(pk=ROOT_NODE_ID)  # type: Part
+        root_node = self.project.part('Product').parent()  # type: Part
         siblings_of_root_node = root_node.siblings()
         assert type(siblings_of_root_node) is PartSet
         assert len(siblings_of_root_node) is 0
@@ -142,40 +134,37 @@ class TestParts(TestBetamax):
     # new in 1.3+
     def test_kwargs_on_part_retrieval(self):
         # test that the additional kwargs are added to the query filter on the api
-        bikes = self.project.parts('Bike', descendants=True)  # type:Part
-        assert len(bikes) > 1
+        parts = self.project.parts('Product', descendants=True)  # type:Part
+        assert len(parts) > 1
         assert self.client.last_url.find('descendants')
 
     # new in 1.5+
     def test_edit_part_instance_name(self):
-        front_fork = self.project.part('Front Fork')
-        front_fork.edit(name='Front Fork - updated')
+        one = self.project.part('One')
+        one.edit(name='One2')
 
-        front_fork_u = self.project.part('Front Fork - updated')
-        assert front_fork.id == front_fork_u.id
-        assert front_fork.name == front_fork_u.name
-        assert front_fork.name == 'Front Fork - updated'
+        one_u = self.project.part(pk=one.id)
+        assert one_u.name == "One2"
 
-        front_fork.edit(name='Front Fork')
+        one_u.edit(name='One')
 
     def test_edit_part_instance_description(self):
-        front_fork = self.project.part('Front Fork')
-        front_fork.edit(description='A normal Front Fork')
+        one = self.project.part('One')
+        one.edit(description='Test Description')
 
-        front_fork_u = self.project.part('Front Fork')
-        assert front_fork.id == front_fork_u.id
+        one_u = self.project.part(pk=one.id)
+        assert one_u._json_data['description'] == 'Test Description'
 
-        front_fork.edit(description='A perfectly normal Front Fork')
+        one_u.edit(description='')
 
     def test_edit_part_model_name(self):
-        front_fork = self.project.model('Front Fork')
-        front_fork.edit(name='Front Fork - updated')
+        one = self.project.model('One')
+        one.edit(name='One2')
 
-        front_fork_u = self.project.model('Front Fork - updated')
-        assert front_fork.id == front_fork_u.id
-        assert front_fork.name == front_fork_u.name
+        one_u = self.project.model(pk=one.id)
+        assert one_u.name == 'One2'
 
-        front_fork.edit(name='Front Fork')
+        one_u.edit(name='One')
 
 
 class TestPartUpdate(TestBetamax):
