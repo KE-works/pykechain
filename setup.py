c536fba6dd4e59f8dbb7f3d26743ca8bc16ab30f
Jochem Berends <jochem.berends@ke-works.com>
2017-04-06 20:00:51 +0200

    updated the requirement 'typing' in the setup.py, updated the changelog and version for a patch release.

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'envparse'],
+    install_requires=['requests', 'envparse', 'typing'],
 
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'betamax'],
