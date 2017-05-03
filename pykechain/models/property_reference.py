dee3c8ce585c467d669afe794f3fd400a2e306ac
Jochem Berends <jochem.berends@ke-works.com>
2017-05-03 14:23:25 +0200

    added docs for the reference property

--- pykechain/models/property_reference.py
+++ pykechain/models/property_reference.py
@@ -12,8 +12,38 @@ class ReferenceProperty(Property):
         """Value of a reference property.
 
         You can set the reference with a Part, Part id or None value.
+        Ensure that the model of the provided part, matches the configured model
 
         :return: Part or None
+        
+        Example
+        -------
+        
+        
+        # get the wheel reference property
+        >>> part = project.part('Bike')
+        >>> material_ref_property = part.property('Material Selection')
+        >>> type(material_ref_property) == ReferenceProperty
+        True
+
+        # the value either returns a Part or is None if not set (yet)
+        >>> type(material_ref_property.value) in (Part, None)
+        True
+        
+        # get the selection of material instances cantaining the word material (icontains is for case-insensitive)
+        >>> material_choices = project.parts(icontains='material')
+        
+        # choose random material option
+        >>> from random import choice
+        >>> chosen_material = choice(material_choices)
+        
+        # set chosen material 
+        # 1: provide the part
+        >>> material_ref_property.value = chosen_material
+        
+        # 2: provide the id
+        >>> material_ref_property.value = chosen_material.id
+               
         """
         if not self._value:
             return None
