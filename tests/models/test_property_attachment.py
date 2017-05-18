8ea29083a2cc6c28e680c89b6c91d118e971d25b
Radu Iordache <radu.iordache@ke-works.com>
2017-05-18 13:33:05 +0200

    #97 increased coverage of property_attachment

--- tests/models/test_property_attachment.py
+++ tests/models/test_property_attachment.py
@@ -1,5 +1,5 @@
 import json
-
+import os
 from tests.classes import TestBetamax
 
 
@@ -14,6 +14,39 @@ class TestAttachment(TestBetamax):
 
         picture._upload(data)
 
-        r = picture._download().json()
+        r = picture.json_load()
 
         self.assertDictEqual(r, self.test_dict)
+
+    def test_retrieve_value(self):
+        photo_attach = self.project.part('Bike').property('Photo Attachment')
+        photo_attach_expected_value = '[Attachment: Awesome.jpg]'
+        photo_attach_actual_value = photo_attach.value
+
+        self.assertEqual(photo_attach_expected_value, photo_attach_actual_value)
+
+        picture = self.project.part('Bike').property('Picture')
+
+        self.assertEqual(picture.value, None)
+
+    def test_set_value(self):
+        picture = self.project.part('Bike').property('Picture')
+
+        with self.assertRaises(RuntimeError):
+            picture.value = 'Should never work'
+
+    def test_upload(self):
+        plot_attach = self.project.part('Bike').property('Plot Attachment')
+
+        # Test the upload of matplotlib figures
+        import matplotlib.pyplot as plt
+        fig = plt.figure(1)
+        plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+        plt.axis([0, 6, 0, 20])
+        plot_attach.upload(fig)
+
+        # Test the upload of regular data (such a .txt file)
+        project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')))
+        requirements = project_root + '/requirements.txt'
+        plot_attach.upload(requirements)
+
