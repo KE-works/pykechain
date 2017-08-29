091265a646b849d98c414d6e20f3d09d9d2c3578
Jochem Berends <jochem.berends@ke-works.com>
2017-08-29 21:16:58 +0200

    fixed tests for the refactoring of scope.members

--- tests/test_scope.py
+++ tests/test_scope.py
@@ -15,12 +15,13 @@ class TestScopes(TestBetamax):
         with self.assertRaises(MultipleFoundError):
             self.client.scope()
 
+    # 1.11+
     def test_retrieve_scope_members(self):
         scope_managers = ['pykechain', 'testmanager', 'jochem.berends']
         scope_members = scope_managers + ['testuser']
 
-        members_usernames = [member['username'] for member in self.project.get_members()]
-        managers_usernames = [manager['username'] for manager in self.project.get_members(is_manager=True)]
+        members_usernames = [member['username'] for member in self.project.members()]
+        managers_usernames = [manager['username'] for manager in self.project.members(is_manager=True)]
 
         self.assertListEqual(sorted(scope_members), sorted(members_usernames))
         self.assertListEqual(sorted(scope_managers), sorted(managers_usernames))
