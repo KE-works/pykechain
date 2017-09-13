8325ff512e2dd6df9fdd1821087cde0c864d5685
Jochem Berends <jochem.berends@ke-works.com>
2017-09-13 13:46:12 +0200

    adding tests to ensure that the provides args are sufficient

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,11 +1,11 @@
-from unittest import TestCase
-
 import os
 
 from pykechain import get_project
 from pykechain.exceptions import ClientError
 from tests.classes import TestBetamax
 from tests.utils import TEST_TOKEN, TEST_URL, TEST_SCOPE_NAME
+
+ERROR_MESSAGE_REGEX = "Error: insufficient arguments"
 
 
 class TestGetProjectHelper(TestBetamax):
@@ -17,9 +17,33 @@ class TestGetProjectHelper(TestBetamax):
         project = get_project(TEST_URL, token=TEST_TOKEN, scope=TEST_SCOPE_NAME)
         self.assertEqual(project.name, TEST_SCOPE_NAME)
 
-    def test_get_project_not_enough_args_raises_error(self):
-        with self.assertRaisesRegex(ClientError, "sufficient arguments"):
+    def test_project_raises_error__no_auth(self):
+        with self.assertRaisesRegex(ClientError, ERROR_MESSAGE_REGEX):
             get_project(url=TEST_URL)
+
+    def test_project_raises_error__token_and_no_scope(self):
+        with self.assertRaisesRegex(ClientError, ERROR_MESSAGE_REGEX):
+            get_project(url=TEST_URL, token=TEST_TOKEN)
+
+    def test_project_raises_error__no_pass(self):
+        with self.assertRaisesRegexp(ClientError, ERROR_MESSAGE_REGEX):
+            get_project(url=TEST_URL, username='auser', scope='Scope')
+
+    def test_project_raises_error__auth_and_no_scope(self):
+        with self.assertRaisesRegexp(ClientError, ERROR_MESSAGE_REGEX):
+            get_project(url=TEST_URL, username='auser', password='somepass')
+
+    def test_project_raises_error__scope_id_and_no_pass(self):
+        with self.assertRaisesRegexp(ClientError, ERROR_MESSAGE_REGEX):
+            get_project(url=TEST_URL, username='auser', scope_id='234')
+
+    def test_project_raises_error__auth_and_no_url(self):
+        with self.assertRaisesRegexp(ClientError, ERROR_MESSAGE_REGEX):
+            get_project(username='auser', password='somepass', scope_id='234')
+
+    def test_project_raises_error__token_and_no_url(self):
+        with self.assertRaisesRegexp(ClientError, ERROR_MESSAGE_REGEX):
+            get_project(token='123', scope_id='234')
 
     def test_get_project_from_env(self):
         # setup
