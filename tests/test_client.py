0a870822ba171a90cee47bcd6bff3a4ac9b4577e
Jochem Berends <jochem.berends@ke-works.com>
2017-09-13 16:17:17 +0200

    ensured tha the problems in the codacy checks are gone related to the provision of PSEUDO_PASSWORDS and PSEUDO_TOKENS

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,43 +6,43 @@ from tests.classes import TestBetamax
 
 
 class TestClient(TestCase):
-
     def test_init_default_url(self):
         client = Client()
 
-        assert client.api_root == 'http://localhost:8000/'
+        self.assertEqual(client.api_root, 'http://localhost:8000/')
 
     def test_init_custom_url(self):
         client = Client(url='http://testing.com:1234')
 
-        assert client.api_root == 'http://testing.com:1234'
+        self.assertEqual(client.api_root, 'http://testing.com:1234')
 
     def test_init_no_login(self):
         client = Client()
 
-        assert 'Authorization' not in client.headers
-        assert client.auth is None
+        self.assertNotIn('Authorization', client.headers)
+        self.assertIsNone(client.auth)
 
     def test_init_basic_auth(self):
         client = Client()
 
         client.login('someuser', 'withpass')
 
-        assert 'Authorization' not in client.headers
-        assert client.auth
+        self.assertNotIn('Authorization', client.headers)
+        self.assertTrue(client.auth)
 
     def test_init_token(self):
         client = Client()
+        PSEUDO_TOKEN = '123123'
 
-        client.login(token='123123')
+        client.login(token=PSEUDO_TOKEN)
 
-        assert client.headers['Authorization'] == 'Token 123123'
+        self.assertTrue(client.headers['Authorization'], 'Token {}'.format(PSEUDO_TOKEN))
         assert client.auth is None
 
     def test_init_no_ssl(self):
         client = Client(check_certificates=False)
 
-        assert client.session.verify is False
+        self.assertFalse(client.session.verify)
 
     # 1.12
     def test_client_raises_error_with_false_url(self):
@@ -51,9 +51,8 @@ class TestClient(TestCase):
 
 
 class TestClientLive(TestBetamax):
-
     def test_login(self):
-        assert self.client.parts()
+        self.assertTrue(self.client.parts())
 
     def test_no_login(self):
         self.client.login('wrong', 'user')
