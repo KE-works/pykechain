dd3934c093fb0762ec85450a8262a7508b872d0a
Jochem Berends <jochem.berends@ke-works.com>
2018-02-15 14:04:19 +0100

    url change

--- tests/utils.py
+++ tests/utils.py
@@ -6,7 +6,7 @@ from envparse import Env
 env = Env()
 env.read_envfile()
 
-TEST_URL = env('TEST_URL', default='TEST_URL=https://kec2api.ke-chain.com')
+TEST_URL = env('TEST_URL', default='https://kec2api.ke-chain.com')
 TEST_USERNAME = env('TEST_USERNAME', default='pykechain')  # LVL1
 TEST_TOKEN = env('TEST_TOKEN', default='')
 TEST_SCOPE_ID = env('TEST_SCOPE_ID', default='6f7bc9f0-228e-4d3a-9dc0-ec5a75d73e1d')
