be662708470ee5d24d4dfaad0040c0c9e939d6f5
radu-iordache <radu.iordache@ke-works.com>
2018-03-06 15:03:20 +0100

    # 292: - fixed the failing tests related to Attachment, Single select and reference properties

--- pykechain/client.py
+++ pykechain/client.py
@@ -830,7 +830,7 @@ class Client(object):
             "name": name,
             "part": model.id,
             "description": description,
-            "property_type": property_type.upper() + '_VALUE',
+            "property_type": property_type.upper(),
             "value": default_value
         }
 
