1e08c2699a3cb0a4ab45fc39ff5ff138002658b7
radu-iordache <radu.iordache@ke-works.com>
2018-03-07 10:55:57 +0100

    # 296:
    - changed all the tests for services to make the call on the scope
    instead of the client such that the functions in scope also get covered.
    Coverage is now 100% in scope

--- tests/test_services.py
+++ tests/test_services.py
@@ -11,18 +11,18 @@ from tests.classes import TestBetamax
 # new in 1.13
 class TestServices(TestBetamax):
     def test_retrieve_services(self):
-        self.assertTrue(self.client.services())
+        self.assertTrue(self.project.services())
 
     def test_retrieve_single_service(self):
-        services = self.client.services()
+        services = self.project.services()
         self.assertTrue(services)
         service_1 = services[0]
 
-        self.assertEqual(self.client.service(pk=service_1.id), service_1)
+        self.assertEqual(self.project.service(pk=service_1.id), service_1)
 
     def test_retrieve_service_by_name(self):
         service_name = 'Debug pykechain'
-        service = self.client.service(name=service_name)
+        service = self.project.service(name=service_name)
         self.assertTrue(service)
         self.assertEqual(service.name, service_name)
 
@@ -30,7 +30,7 @@ class TestServices(TestBetamax):
                         reason="Skipping tests when using Travis, as Service Execution cannot be provided")
     def test_debug_service_execute(self):
         service_name = 'Debug pykechain'
-        service = self.client.service(name=service_name)
+        service = self.project.service(name=service_name)
 
         service_execution = service.execute()
         self.assertTrue(service_execution.status in ServiceExecutionStatus.values())
@@ -45,7 +45,7 @@ class TestServices(TestBetamax):
 
     def test_update_service(self):
         service_name = 'Debug pykechain'
-        service = self.client.service(name=service_name)
+        service = self.project.service(name=service_name)
         version_before = str(service.version)
 
         version_after = '-latest'
@@ -57,7 +57,7 @@ class TestServices(TestBetamax):
 
     def test_service_refresh_from_kechain(self):
         service_name = 'Debug pykechain'
-        service = self.client.service(name=service_name)
+        service = self.project.service(name=service_name)
         version_before = str(service.version)
 
         version_after = '-latest'
@@ -70,32 +70,32 @@ class TestServices(TestBetamax):
 
     def test_create_and_delete_service(self):
         service_name = 'Test service creation'
-        new_service = self.client.create_service(service_name, scope=self.project.id)
+        new_service = self.project.create_service(service_name)
 
         self.assertTrue(new_service.name, service_name)
         self.assertTrue(new_service)
 
-        # destroy
+        # tearDown
         new_service.delete()
         with self.assertRaisesRegex(NotFoundError, 'No service fits criteria'):
-            self.client.service(pk=new_service.id)
+            self.project.service(pk=new_service.id)
 
 # new in 1.13class TestServiceExecutions(TestBetamax):
     def test_retrieve_service_executions(self):
-        self.assertTrue(self.client.service_executions())
+        self.assertTrue(self.project.service_executions())
 
     def test_retrieve_single_service_execution(self):
-        service_executions = self.client.service_executions()
+        service_executions = self.project.service_executions()
         self.assertTrue(service_executions)
         service_execution_1 = service_executions[0]
 
-        self.assertEqual(self.client.service_execution(pk=service_execution_1.id), service_execution_1)
+        self.assertEqual(self.project.service_execution(pk=service_execution_1.id), service_execution_1)
 
     @pytest.mark.skipif("os.getenv('TRAVIS', False)",
                         reason="Skipping tests when using Travis, as Service Execution cannot be provided")
     def test_debug_service_execution_terminate(self):
         service_name = 'Debug pykechain with 10s load'
-        service = self.client.service(name=service_name)
+        service = self.project.service(name=service_name)
 
         service_execution = service.execute()
         service_execution.refresh()
