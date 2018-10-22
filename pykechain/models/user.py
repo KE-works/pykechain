2e2f9d6c134ee242aaf0a5abd83ebaab9a44cac5
raduiordache <radu.iordache@ke.works.com>
2018-10-22 11:55:21 +0200

    Add timezone and language preference in User object:
    - added them as properties
    - wrote tests (coverage=100%)
    - fixed also #373

--- pykechain/models/user.py
+++ pykechain/models/user.py
@@ -1,3 +1,5 @@
+import pytz
+
 from .base import Base
 
 
@@ -5,8 +7,17 @@ class User(Base):
     """A virtual object representing a KE-chain user.
 
     :ivar username: username of the user
+    :type username: str
     :ivar name: username of the user (compatibility)
+    :type name: str
     :ivar id: userid of the user
+    :type id: int
+    :ivar timezone: timezone of the User
+    :type timezone: timezone object
+    :ivar language: language of the User
+    :type language: str
+    :ivar email: email of the User
+    :type email: str
     """
 
     def __init__(self, json, **kwargs):
@@ -14,8 +25,20 @@ class User(Base):
         super(User, self).__init__(json, **kwargs)
 
         self.username = self._json_data.get('username', '')
-        self.name = self.username
+        self.name = self.name
         self.id = self._json_data.get('pk', '')
 
     def __repr__(self):  # pragma: no cover
         return "<pyke {} '{}' id {}>".format(self.__class__.__name__, self.username, self.id)
+
+    @property
+    def timezone(self):
+        return pytz.timezone(zone=self._json_data.get('timezone', 'UTC'))
+
+    @property
+    def language(self):
+        return self._json_data.get('language_code', 'en')
+
+    @property
+    def email(self):
+        return self._json_data.get('email', '')
