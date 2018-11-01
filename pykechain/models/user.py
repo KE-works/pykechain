89caec628433a65931f7c36904d3638e65de86b6
Jochem Berends <jochem.berends@ke-works.com>
2018-11-01 14:40:21 +0100

    due to newer flake8, ignoring the unused typing imports

--- pykechain/models/user.py
+++ pykechain/models/user.py
@@ -12,11 +12,11 @@ class User(Base):
     :type name: str
     :ivar id: userid of the user
     :type id: int
-    :ivar timezone: timezone of the User
+    :ivar timezone: timezone of the User (defaults to <UTC>)
     :type timezone: timezone object
-    :ivar language: language of the User
+    :ivar language: language of the User (defaults to 'en')
     :type language: str
-    :ivar email: email of the User
+    :ivar email: email of the User (defaults to '')
     :type email: str
     """
 
@@ -33,13 +33,35 @@ class User(Base):
 
     @property
     def timezone(self):
+        """
+        Timezone of the user.
+
+        Defaults to timezone UTC.
+        With return a pytz timezone eg. 'Europe/Amsterdam'
+
+        :return: timezone object (compatible with datetime)
+        :rtype: TzInfo
+        """
         return pytz.timezone(zone=self._json_data.get('timezone', 'UTC'))
 
     @property
     def language(self):
+        """
+        Language code of the user.
+
+        Defaults to English ('en") when no language code is configured.
+
+        :return: language code string
+        :rtype: basestring
+        """
         return self._json_data.get('language_code', 'en')
 
     @property
     def email(self):
+        """
+        Email of the user.
+
+        :return: email address, default is empty string.
+        :rtype: basestring
+        """
         return self._json_data.get('email', '')
-    
