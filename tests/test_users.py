32bac20357ba56a661cb723e590e28a020cccacc
Radu Iordache <radu.iordache@ke-works.com>
2018-11-01 14:47:26 +0100

    Add timezone and language preference in User object: (#402)
    * Add timezone and language preference in User object:
    
    - added them as properties
    - wrote tests (coverage=100%)
    - fixed also #373
    
    * removing python 3.8 checks
    
    * Update user.py
    
    newline added
    
    * due to newer flake8, ignoring the unused typing imports

--- tests/test_users.py
+++ tests/test_users.py
@@ -15,5 +15,28 @@ class TestUsers(TestBetamax):
         with self.assertRaises(MultipleFoundError):
             self.client.user()
 
-    def test_retrieve_single_user_with_known_username(self):
+    def test_retrieve_single_user_with_known_id(self):
         self.assertTrue(self.client.user(id=1))
+
+    def test_retrieve_single_user_with_known_username(self):
+        self.assertTrue(self.client.user(username='testuser'))
+
+    def test_retrieve_user_timezone(self):
+        user_retrieved = self.client.user(username='testuser')
+        timezone_retrieved = user_retrieved.timezone
+        self.assertEqual(timezone_retrieved.zone, 'Atlantic/Cape_Verde')
+
+    def test_retrieve_user_name(self):
+        user_retrieved = self.client.user(username='testuser')
+        name_retrieved = user_retrieved.name
+        self.assertEqual(name_retrieved, 'User Test')
+
+    def test_retrieve_user_email(self):
+        user_retrieved = self.client.user(username='testuser')
+        email_retrieved = user_retrieved.email
+        self.assertEqual(email_retrieved, 'a@b.nl')
+
+    def test_retrieve_user_language(self):
+        user_retrieved = self.client.user(username='testuser')
+        language_retrieved = user_retrieved.language
+        self.assertEqual(language_retrieved, 'fr')
