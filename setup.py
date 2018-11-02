fb50ec843d21e0ed13945f286f78fce8769b0ceb
Jochem Berends <jochem.berends@ke-works.com>
2018-11-02 13:00:30 +0100

    adding pytz to the default dependencies

--- setup.py
+++ setup.py
@@ -87,10 +87,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'envparse', 'typing', 'six', 'jsonschema', 'semver'],
+    install_requires=['requests', 'envparse', 'typing', 'six', 'jsonschema', 'semver', 'pytz'],
 
     setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytz', 'betamax', 'six'],
+    tests_require=['pytest', 'betamax'],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
