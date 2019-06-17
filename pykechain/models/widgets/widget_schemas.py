f68ccd18cd85406b0079f3414da324e5d28c2978
Jochem Berends <jochem.berends@ke-works.com>
2019-06-17 17:32:19 +0200

    adding an initial schema

---
+++ pykechain/models/widgets/widget_schemas.py
@@ -0,0 +1,11 @@
+
+
+
+
+widget_meta_schema = {
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "title": "Toplevel Widget Meta schema",
+    "type": "object",
+    "additionalProperties": True,
+    "properties": {}
+}
