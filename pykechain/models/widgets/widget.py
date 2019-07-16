49fc87577687e4c7446deeea020268b7e8e79df3
Jochem Berends <jochem.berends@ke-works.com>
2019-07-16 17:45:17 +0200

    fixed edit widget; added url params

--- pykechain/models/widgets/widget.py
+++ pykechain/models/widgets/widget.py
@@ -161,8 +161,9 @@ class Widget(Base):
         if kwargs:
             update_dict.update(**kwargs)
 
+        from pykechain.client import API_EXTRA_PARAMS
         url = self._client._build_url('widget', widget_id=self.id)
-        response = self._client._request('PUT', url, json=update_dict)
+        response = self._client._request('PUT', url, params=API_EXTRA_PARAMS['widgets'], json=update_dict)
 
         if response.status_code != requests.codes.ok:  # pragma: no cover
             raise APIError("Could not update Widget ({})".format(response))
