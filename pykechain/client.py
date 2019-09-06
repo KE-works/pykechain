f46c96cf428a6be62082ed79176f7c97156c40cb
Bastiaan Beijer <bastiaan.beijer@ke-works.com>
2019-09-06 11:01:15 +0200

    Reverted removal of all attributes for which the value is `None`. Fix has been added to the schema validation side in KE-chain core

--- pykechain/client.py
+++ pykechain/client.py
@@ -2092,11 +2092,6 @@ class Client(object):
                 else:
                     IllegalArgumentError("`writable_models` should be provided as a list of uuids or property models")
 
-        # remove 'None' values from the meta
-        for key in set(meta.keys()):
-            if meta.get(key) is None:
-                del meta[key]
-
         data = dict(
             activity_id=activity,
             widget_type=widget_type,
