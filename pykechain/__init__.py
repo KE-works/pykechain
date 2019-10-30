4cdd9bbea13545a0b7bde094b704060d970f8d94
Jochem Berends <jochem.berends@ke-works.com>
2019-10-30 11:22:22 +0100

    making pykechain only operate on python version 3

--- pykechain/__init__.py
+++ pykechain/__init__.py
@@ -1,4 +1,7 @@
 """A python library to connect and interact with KE-chain."""
+import sys
+
+import six
 
 from .__about__ import version
 from .client import Client
@@ -7,3 +10,7 @@ from .helpers import get_project
 __all__ = (
     'Client', 'get_project', 'version'
 )
+
+if six.PY2 or (sys.version_info.major == 3 and sys.version_info.minor <= 4):
+    raise RuntimeError("Python version >= `3.5` is required for this version of `pykechain` to operate. "
+                       "Please use `pykechain` version < `3.0` for usage in combination with Python `2.7`")
