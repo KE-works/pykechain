8cef8b4b154ce0b096c85a09521dfac52112866c
Jochem Berends <jochem.berends@ke-works.com>
2020-03-27 09:22:09 +0100

    improved email validator and added test. ready for patch release 3.6.1

--- pykechain/utils.py
+++ pykechain/utils.py
@@ -8,11 +8,17 @@ from typing import TypeVar, Iterable, Callable, Optional, Text, Dict  # noqa: F4
 import pytz
 import six
 
-T = TypeVar('T')
+T = TypeVar("T")
 
-UUID_REGEX_PATTERN = r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
-EMAIL_REGEX_PATTERN = r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@" \
-                      r"(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+UUID_REGEX_PATTERN = (
+    r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+)
+EMAIL_REGEX_PATTERN = (
+    r"^([-!#$%&'*+/=?^_`{}|~0-9a-zA-Z]+"
+    r"(\.[-!#$%&'*+/=?^_`{}|~0-9a-zA-Z]+)*|^\""
+    r"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*\")"
+    r"@(?:[a-zA-Z0-9\[\]](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z0-9\[\]]{2,6}\.?$"
+)
 
 
 def find(iterable: Iterable[T], predicate: Callable[[T], bool]) -> Optional[T]:
@@ -90,9 +96,7 @@ def is_url(value: Text) -> bool:
         r"(?:172\.(?:1[6-9]|2\d|3[0-1])" + ip_middle_octet + ip_last_octet + r"))"
         r"|"
         # private & local hosts
-        r"(?P<private_host>"
-        r"(?:localhost))"
-        r"|"
+        r"(?P<private_host>" r"(?:localhost))" r"|"
         # IP address dotted notation octets
         # excludes loopback network 0.0.0.0
         # excludes reserved space >= 224.0.0.0
@@ -135,15 +139,13 @@ def is_url(value: Text) -> bool:
         r"((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}"
         # 2001:db8:3:4::192.0.2.33  64:ff9b::192.0.2.33
         # (IPv4-Embedded IPv6 Address)
-        r"(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])"
-        r")\]|"
+        r"(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])" r")\]|"
         # host name
         u"(?:(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)"
         # domain name
         u"(?:\.(?:[a-z\u00a1-\uffff0-9]-?)*[a-z\u00a1-\uffff0-9]+)*"
         # TLD identifier
-        u"(?:\.(?:[a-z\u00a1-\uffff]{2,}))"
-        r")"
+        u"(?:\.(?:[a-z\u00a1-\uffff]{2,}))" r")"
         # port number
         r"(?::\d{2,5})?"
         # resource path
@@ -151,9 +153,8 @@ def is_url(value: Text) -> bool:
         # query string
         r"(?:\?\S*)?"
         # fragment
-        r"(?:#\S*)?"
-        r"$",
-        re.UNICODE | re.IGNORECASE
+        r"(?:#\S*)?" r"$",
+        re.UNICODE | re.IGNORECASE,
     )
     result = regex.match(value)
     if result:
@@ -214,21 +215,23 @@ def temp_chdir(cwd: Optional[Text] = None):
     """
     if six.PY3:
         from tempfile import TemporaryDirectory
+
         with TemporaryDirectory() as tempwd:
             origin = cwd or os.getcwd()
             os.chdir(tempwd)
 
             try:
-                yield tempwd if os.path.exists(tempwd) else ''
+                yield tempwd if os.path.exists(tempwd) else ""
             finally:
                 os.chdir(origin)
     else:
         from tempfile import mkdtemp
+
         tempwd = mkdtemp()
         origin = cwd or os.getcwd()
         os.chdir(tempwd)
         try:
-            yield tempwd if os.path.exists(tempwd) else ''
+            yield tempwd if os.path.exists(tempwd) else ""
         finally:
             os.chdir(origin)
 
@@ -263,34 +266,34 @@ def parse_datetime(value: Text) -> Optional[datetime]:
         """Return a tzinfo instance with a fixed offset from UTC."""
         if isinstance(offset, timedelta):
             offset = offset.total_seconds() // 60
-        sign = '-' if offset < 0 else '+'
-        hhmm = '%02d%02d' % divmod(abs(offset), 60)
+        sign = "-" if offset < 0 else "+"
+        hhmm = "%02d%02d" % divmod(abs(offset), 60)
         name = sign + hhmm
         return timezone(timedelta(minutes=offset), name)
 
     DATETIME_RE = re.compile(
-        r'(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})'
-        r'[T ](?P<hour>\d{1,2}):(?P<minute>\d{1,2})'
-        r'(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?'
-        r'(?P<tzinfo>Z|[+-]\d{2}(?::?\d{2})?)?$'
+        r"(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})"
+        r"[T ](?P<hour>\d{1,2}):(?P<minute>\d{1,2})"
+        r"(?::(?P<second>\d{1,2})(?:\.(?P<microsecond>\d{1,6})\d{0,6})?)?"
+        r"(?P<tzinfo>Z|[+-]\d{2}(?::?\d{2})?)?$"
     )
 
     match = DATETIME_RE.match(value)
     if match:
         kw = match.groupdict()
-        if kw['microsecond']:
-            kw['microsecond'] = kw['microsecond'].ljust(6, '0')
-        tzinfo = kw.pop('tzinfo')
-        if tzinfo == 'Z':
+        if kw["microsecond"]:
+            kw["microsecond"] = kw["microsecond"].ljust(6, "0")
+        tzinfo = kw.pop("tzinfo")
+        if tzinfo == "Z":
             tzinfo = pytz.UTC
         elif tzinfo is not None:
             offset_mins = int(tzinfo[-2:]) if len(tzinfo) > 3 else 0
             offset = 60 * int(tzinfo[1:3]) + offset_mins
-            if tzinfo[0] == '-':
+            if tzinfo[0] == "-":
                 offset = -offset
             tzinfo = _get_fixed_timezone(offset)
         kw = {k: int(v) for k, v in six.iteritems(kw) if v is not None}
-        kw['tzinfo'] = tzinfo
+        kw["tzinfo"] = tzinfo
         return datetime(**kw)
 
 
@@ -298,6 +301,7 @@ def parse_datetime(value: Text) -> Optional[datetime]:
 # The following functions are inspired by: https://github.com/okunishinishi/python-stringcase
 # License: MIT
 #
+
 
 def camelcase(string: Text) -> Text:
     """Convert string into camel case.
@@ -316,10 +320,12 @@ def camelcase(string: Text) -> Text:
     fooBarBaz
 
     """
-    string = re.sub(r"^[\-_\.]", '', str(string))
+    string = re.sub(r"^[\-_\.]", "", str(string))
     if not string:
         return string
-    return lowercase(string[0]) + re.sub(r"[\-_\.\s]([a-z])", lambda matched: uppercase(matched.group(1)), string[1:])
+    return lowercase(string[0]) + re.sub(
+        r"[\-_\.\s]([a-z])", lambda matched: uppercase(matched.group(1)), string[1:]
+    )
 
 
 def capitalcase(string: Text) -> Text:
@@ -388,10 +394,12 @@ def snakecase(string: Text) -> Text:
     foo_bar_baz
 
     """
-    string = re.sub(r"[\-\.\s]", '_', str(string))
+    string = re.sub(r"[\-\.\s]", "_", str(string))
     if not string:
         return string
-    return lowercase(string[0]) + re.sub(r"[A-Z]", lambda matched: '_' + lowercase(matched.group(0)), string[1:])
+    return lowercase(string[0]) + re.sub(
+        r"[A-Z]", lambda matched: "_" + lowercase(matched.group(0)), string[1:]
+    )
 
 
 def uppercase(string: Text) -> Text:
@@ -426,12 +434,14 @@ def slugify_ref(value: Text, allow_unicode: bool = False) -> Text:
     :return: slugified value
     """
     if allow_unicode:
-        value = unicodedata.normalize('NFKC', value)
-        value = re.sub(r'[^\w\s-]', '', value, flags=re.U).strip().lower()
-        return re.sub(r'[-\s]+', '-', value, flags=re.U)
-    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
-    value = re.sub(r'[^\w\s-]', '', value).strip().lower()
-    return re.sub(r'[-\s]+', '-', value)
+        value = unicodedata.normalize("NFKC", value)
+        value = re.sub(r"[^\w\s-]", "", value, flags=re.U).strip().lower()
+        return re.sub(r"[-\s]+", "-", value, flags=re.U)
+    value = (
+        unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
+    )
+    value = re.sub(r"[^\w\s-]", "", value).strip().lower()
+    return re.sub(r"[-\s]+", "-", value)
 
 
 def __dict_public__(cls):
@@ -449,7 +459,7 @@ def __dict_public__(cls):
     ['INSTANCE', 'MODEL']
 
     """
-    return {k: v for (k, v) in cls.__dict__.items() if not k.startswith('__')}
+    return {k: v for (k, v) in cls.__dict__.items() if not k.startswith("__")}
 
 
 def __dict__inherited__(cls, stop=type, public=True):
