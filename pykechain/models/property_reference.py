9100186c295563813917ffaf1e7de6c663f49bba
Radu Iordache <radu.iordache@ke-works.com>
2020-08-31 09:19:07 +0200

    Add ScopeReferenceProperty and UserReferenceProperty (#835)
    * - added ScopeReferenceProperty;
    - added UserReferenceProperty;
    - added tests and cassettes;
    - ran black on the changed files;
    - added changelog;
    
    * fixed a failing test
    
    * fixed a failing test
    
    * increased the coverage
    
    * Implemented feedback from Jochem:
    - renamed all Scope, User and Activity Reference property to ReferenceS property

--- pykechain/models/property_reference.py
+++ pykechain/models/property_reference.py
@@ -1,11 +1,11 @@
 from typing import List
 
-from pykechain.models import Activity
+from pykechain.models import Activity, Scope, user
 from pykechain.models.base_reference import _ReferencePropertyInScope
 
 
-class ActivityReferenceProperty(_ReferencePropertyInScope):
-    """A virtual object representing a KE-chain Activity Reference property.
+class ActivityReferencesProperty(_ReferencePropertyInScope):
+    """A virtual object representing a KE-chain Activity References property.
 
     .. versionadded:: 3.7
     """
@@ -25,3 +25,72 @@ class ActivityReferenceProperty(_ReferencePropertyInScope):
             activity.refresh()  # To populate the object with all expected data
             activities.append(activity)
         return activities
+
+
+class ScopeReferencesProperty(_ReferencePropertyInScope):
+    """A virtual object representing a KE-chain Scope References property.
+
+    .. versionadded: 3.9
+    """
+
+    REFERENCED_CLASS = Scope
+
+    def _retrieve_objects(self, **kwargs) -> List[Scope]:
+        """
+        Retrieve a list of Scopes.
+
+        :param kwargs: optional inputs
+        :return: list of Scope2 objects
+        """
+        if not self._value:
+            return []
+        scope_ids = []
+        for value in self._value:
+            if isinstance(value, dict):
+                scope_ids.append(value.get("id"))
+            elif isinstance(value, str):
+                scope_ids.append(value)
+            else:  # pragma: no cover
+                raise ValueError(
+                    'Value "{}" must be a dict with field `id` or a UUID.'.format(value)
+                )
+
+        scopes = []
+        if scope_ids:
+            scopes = list(self._client.scopes(id__in=",".join(scope_ids)))
+        return scopes
+
+
+class UserReferencesProperty(_ReferencePropertyInScope):
+    """A virtual object representing a KE-chain User References property.
+
+    .. versionadded: 3.9
+    """
+
+    REFERENCED_CLASS = user.User
+
+    def _retrieve_objects(self, **kwargs) -> List[user.User]:
+        """
+        Retrieve a list of Users.
+
+        :param kwargs: optional inputs
+        :return: list of User objects
+
+        """
+        if not self._value:
+            return []
+        users_ids = []
+        for value in self._value:
+            if isinstance(value, dict) and "pk" in value:
+                users_ids.append(str(value.get("pk")))
+            elif isinstance(value, (int, str)):
+                users_ids.append(str(value))
+            else:  # pragma: no cover
+                raise ValueError(
+                    'Value "{}" must be a dict with field `pk` or a UUID.'.format(value)
+                )
+
+        users = []
+        if users_ids:
+            users = list(self._client.users(id__in=",".join(users_ids)))
+        return users
