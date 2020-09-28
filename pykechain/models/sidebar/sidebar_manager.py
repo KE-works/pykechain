71ffc8359453d5e06c731d4c908e75a269616d8f
jelle.boersma <jelle.boersma@ke-works.com>
2020-09-28 14:21:26 +0200

    Documentation

--- pykechain/models/sidebar/sidebar_manager.py
+++ pykechain/models/sidebar/sidebar_manager.py
@@ -104,7 +104,8 @@ class SideBarManager(Iterable):
         self._perform_bulk_creation = False
         self._update()
 
-    def refresh(self):
+    def refresh(self) -> None:
+        """Reload the scope options from KE-chain to refresh the side-bar data."""
         self.scope.refresh()
         self.__init__(scope=self.scope)
 
