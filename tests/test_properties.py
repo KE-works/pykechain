740cd417c286ec682d787ab9460e951d81684da1
JelleBoersma <41430092+JelleBoersma@users.noreply.github.com>
2020-11-24 14:25:26 +0100

    Lean child() method on Activity (#891)
    * The `child()` method of the `Activity` class now tries to find a cached child prior to requesting the child from KE-chain.
    * Bugfix, Property class cant access the Property instance value. Also capitalized the class attribute.

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -443,7 +443,7 @@ class TestUpdateProperties(TestBetamax):
         self.assertIsNone(self._refresh_prop(self.prop_1).value)
         Property.update_values(client=self.client)
         self.assertEqual(self._refresh_prop(self.prop_1).value, 'new value')
-        self.assertFalse(Property._use_bulk_update)
+        self.assertFalse(Property._USE_BULK_UPDATE)
 
         # setUp 2
         Property.set_bulk_update(True)
@@ -453,7 +453,7 @@ class TestUpdateProperties(TestBetamax):
         self.assertIsNone(self._refresh_prop(self.prop_2).value)
         Property.update_values(client=self.client, use_bulk_update=True)
         self.assertEqual(self._refresh_prop(self.prop_2).value, 'another value')
-        self.assertTrue(Property._use_bulk_update)
+        self.assertTrue(Property._USE_BULK_UPDATE)
 
         # tearDown
         Property.set_bulk_update(False)
