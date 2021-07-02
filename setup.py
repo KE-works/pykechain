4f26b430660cdc4b613480f4d224448d66e8cb06
Jochem Berends <jochem.berends@ke-works.com>
2021-07-02 14:19:13 +0200

    version and changelog

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "betamax"],
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
