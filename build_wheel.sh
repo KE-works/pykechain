99c487695918b30e003fd299b2962f798316e34f
Jochem Berends <jochem.berends@ke-works.com>
2025-05-19 10:03:04 +0200

    Remove deprecated files and configurations; migrate to pyproject.toml for project metadata and dependencies. Deleted .pre-commit-config.yaml, build_wheel.sh, setup.py, setup.cfg, mypy.ini, and tox.ini. Updated MANIFEST.in and requirements.txt for improved packaging and dependency management. Adjusted GitHub workflows for building and testing to align with new structure.

--- build_wheel.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf ./build ./dist
-python setup.py sdist bdist_wheel --universal
-echo "--- uploading to PyPI with either your username or username=__token__ and an API token recorded."
-twine upload dist/pykechain-*
